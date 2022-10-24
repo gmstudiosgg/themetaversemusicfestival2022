f96d233af5418e0525e5c056d8ffe90a81b20c9b
franzo <franciscoaboy@gmail.com>
2022-10-24 18:33:48 -0300

    Feature/footer & navbar(#5)
    * Replace logo asset
    
    * add star icon asset
    
    * Hero styles
    
    * Remove video banner
    
    * Add stars
    
    * Adjust fonts
    
    * remove imports & code style
    
    * social links src changes
    
    * Navbar styles & change incons vectors
    
    * change logo metallic
    
    * correction line up link label
    
    * Add Navbar links hover color
    
    * Add Footer Component / change vector files to js
    
    * Update Navbar icons
    
    Co-authored-by: fede-blanco <blancofedem@gmail.com>

---
+++ src/components/Hero.jsx
@@ -0,0 +1,206 @@
+import React from "react";
+import styled from "styled-components";
+import Button from "./Button";
+import theme, { breakpoints } from "../../utils/theme";
+import mvmfLogo from "../images/logo-metaverse.png";
+import dclLogo from "../images/logo-dcl.svg";
+import star from "../images/star.svg";
+import whiteRabbit from "../images/rabbit.svg";
+import polygon from "../images/STD-polygon.svg";
+import { Container } from "./Container";
+
+const Hero = () => {
+  return (
+    <StyledHero sx={{ height: "100vh", display: "flex", alignItems: "center" }}>
+      <Container>
+        <LogoContainer>
+          <StyledImage
+            placeholder={"none"}
+            backgroundColor={"black"}
+            objectFit={"contain"}
+            loading={"eager"}
+            className="index-hero-logo"
+            alt={"Metaverse Music Festival"}
+            src={mvmfLogo}
+          />
+        </LogoContainer>
+        <EventDetails>
+          <EventData>
+            <EventDataItem>
+              <DateComponent className="event-data">
+                <p>
+                  NOV. 10-13 • <DateHighlight>-12PM UTC</DateHighlight> •
+                </p>
+                <DateLocation>FESTIVAL LAND, [-62,70]</DateLocation>
+              </DateComponent>
+            </EventDataItem>
+          </EventData>
+          <EventDataBottom>
+            <EventDataItem>
+              <a href="https://play.decentraland.org/" target="_blank">
+                <StyledLogo alt={"Decentraland Logo"} src={dclLogo} />
+              </a>
+            </EventDataItem>
+            <EventDataItem>
+              <SaveTheDate
+                href="https://events.decentraland.org/event/?id=7148716b-ff25-4d5f-8267-55fed7ab15bc"
+                target="_blank"
+              >
+                <WhiteRabbit src={whiteRabbit} /> Save the date{" "}
+                <Polygon src={polygon} />
+              </SaveTheDate>
+            </EventDataItem>
+          </EventDataBottom>
+          <Decorator
+            src={star}
+            top="50%"
+            left="-60px"
+            width="22px"
+            height="62px"
+          />
+          <Decorator
+            src={star}
+            top="50%"
+            right="-60px"
+            width="22px"
+            height="62px"
+          />
+        </EventDetails>
+      </Container>
+    </StyledHero>
+  );
+};
+
+const StyledHero = styled.div`
+  position: relative;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+export const LogoContainer = styled.div`
+  padding: 0 66px;
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  height: auto;
+`;
+
+const EventDetails = styled.div`
+  position: relative;
+  padding: 0 121px;
+  border-left: 3px solid white;
+  border-right: 3px solid white;
+`;
+
+const EventData = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 9px;
+  @media screen and (max-width: ${breakpoints.md}) {
+    flex-direction: column;
+  }
+`;
+
+const EventDataBottom = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 32px;
+  @media screen and (max-width: ${breakpoints.md}) {
+    flex-direction: column;
+  }
+`;
+
+const EventDataItem = styled.div`
+  flex: 0 0 100%;
+  width: 100%;
+  text-align: center;
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    flex: 1 0 50%;
+    text-align: left;
+  }
+`;
+
+const DateComponent = styled.section`
+  font-size: 26px;
+  font-weight: 500;
+  line-height: 88%;
+  font-family: "Yapari Expanded";
+  padding-left: 0;
+  width: 100%;
+  text-align: center;
+  letter-spacing: 14px;
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    font-size: 18px;
+    text-align: center;
+  }
+
+  @media screen and (min-width: ${breakpoints.l}) {
+    font-size: 26px;
+    line-height: 88%;
+    width: 100%;
+  }
+`;
+
+const DateHighlight = styled.span`
+  color: ${theme.accent};
+`;
+
+const DateLocation = styled.span`
+  font-family: "Yapari";
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 72px;
+  letter-spacing: 7px;
+`;
+
+const StyledLogo = styled.img`
+  max-width: 70%;
+
+  @media screen and (max-width: ${breakpoints.md}) {
+    max-width: 50%;
+    margin: 0 auto;
+    text-align: center;
+  }
+`;
+
+const SaveTheDate = styled(Button)`
+  width: 100%;
+  background-image: url(../images/boton.png);
+`;
+
+const Decorator = styled.img`
+  position: absolute;
+  transform: translateY(-50%);
+  width: ${(props) => `${props.width}`};
+  height: ${(props) => `${props.height}`};
+  left: ${(props) => `${props.left}` || null};
+  top: ${(props) => `${props.top}` || null};
+  bottom: ${(props) => `${props.bottom}` || null};
+  right: ${(props) => `${props.right}` || null};
+`;
+
+const WhiteRabbit = styled.img`
+  width: 52px;
+  height: 30px;
+  box-sizing: border-box;
+  padding-top: 4px;
+  padding-bottom: 4px;
+  padding-right: 16px;
+  margin-right: 16px;
+  border-right: 4px solid ${theme.accent};
+`;
+
+const Polygon = styled.img`
+  width: 15px;
+  height: 15px;
+  margin-left: 16px;
+`;
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,242 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import theme, { breakpoints } from "../../utils/theme";
+import { StaticImage } from "gatsby-plugin-image";
+import { Twitter } from "../components/icons/Vector-Twitter.js";
+import { Discord } from "../components/icons/Vector-Discord.js";
+import { OpenSea } from "../components/icons/Vector-openSea.js";
+
+const Navbar = (props) => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const Links = [
+    {
+      label: "ABOUT",
+      targetId: "about",
+    },
+    {
+      label: "LINE UP",
+      targetId: "lineup",
+    },
+    {
+      label: "MAP",
+      targetId: "map",
+    },
+    {
+      label: "FAQ",
+      targetId: "faq",
+    },
+    // {
+    //   label: 'SPONSORS',
+    //   targetId: 'sponsors'
+    // },
+  ];
+
+  const scrollTo = (targetId) => {
+    const target = document.getElementById(targetId);
+    if (target) {
+      target.scrollIntoView();
+    }
+  };
+  return (
+    <>
+      <StyledNavbar className="navbar">
+        <Nav>
+          <div>
+            <StaticImage src={"./../images/logo.svg"} height={140} />
+          </div>
+
+          <div>
+            <MenuList>
+              {Links.map((link, i) => (
+                <MenuItem key={i}>
+                  <a
+                    onClick={() => {
+                      scrollTo(link.targetId);
+                    }}
+                  >
+                    {link.label}
+                  </a>
+                </MenuItem>
+              ))}
+            </MenuList>
+          </div>
+          <div>
+            <SocialIcon
+              href="https://twitter.com/decentraland"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Twitter />
+            </SocialIcon>
+            <SocialIcon
+              href="https://decentraland.org/discord/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Discord />
+            </SocialIcon>
+            <SocialIcon
+              href="https://www.instagram.com/decentraland_foundation/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <OpenSea />
+            </SocialIcon>
+          </div>
+        </Nav>
+        <div
+          className="mobile-toggle"
+          onClick={() => {
+            setIsMobileMenuOpen(!isMobileMenuOpen);
+          }}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`mobile-navbar ${isMobileMenuOpen ? "open" : ""}`}>
+          <StaticImage
+            onClick={() => {
+              setIsMobileMenuOpen(!isMobileMenuOpen);
+            }}
+            className="close-menu-icon"
+            src={"./../images/close_menu_icon.png"}
+            alt="close"
+            title="close"
+          />
+          <ul>
+            {Links.map((link, i) => (
+              <li key={i}>
+                <a
+                  onClick={() => {
+                    scrollTo(link.targetId);
+                    setIsMobileMenuOpen(!isMobileMenuOpen);
+                  }}
+                >
+                  {link.label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </StyledNavbar>
+    </>
+  );
+};
+
+const StyledNavbar = styled.section`
+  width: 100%;
+  z-index: 100;
+  position: fixed;
+  background-color: black;
+
+  /* Agregado fede */
+  border-bottom: solid 5px ${theme.accent};
+  display: flex;
+  justify-content: space-between;
+  /* ---------------- */
+
+  @media screen and (max-width: ${breakpoints.md}) {
+    bottom: 0;
+    height: 52px;
+    background: #eefe56;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  &:before {
+    position: absolute;
+    bottom: 0;
+    height: 1px;
+    width: 100%;
+    content: "";
+    @media screen and (max-width: ${breakpoints.md}) {
+      display: none;
+    }
+  }
+  .mobile-toggle {
+    display: none;
+    width: 80px;
+    @media screen and (max-width: ${breakpoints.md}) {
+      display: unset;
+    }
+    div {
+      width: 80px;
+      height: 1px;
+      background: black;
+      margin-bottom: 10px;
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+  .mobile-navbar {
+    width: 100vw;
+    height: 100vh;
+    bottom: -100vh;
+    position: absolute;
+    background: #eefe56;
+    opacity: 0;
+    pointer-events: none;
+    transition: 0.5s ease-in-out all;
+    padding: 24px;
+    .close-menu-icon {
+      width: 50px;
+      margin-bottom: 30px;
+    }
+    &.open {
+      bottom: 0;
+      opacity: 1;
+      pointer-events: unset;
+    }
+  }
+`;
+
+const SocialIcon = styled.a`
+  margin-left: 24px;
+  svg {
+    width: 24px;
+    height: 24px;
+
+    path {
+      transition: fill 0.4s ease;
+    }
+
+    :hover {
+      path {
+        fill: ${theme.accent};
+      }
+    }
+  }
+`;
+
+const Nav = styled.nav`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 18px 62px;
+`;
+
+const MenuItem = styled.li`
+  font-family: "Yapari";
+  font-size: 16px;
+  font-weight: 700;
+  padding-left: 40px;
+  letter-spacing: 2px;
+
+  a {
+    transition: color 0.5s ease;
+  }
+
+  a:hover {
+    color: ${theme.accent};
+  }
+`;
+
+const MenuList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export default Navbar;
