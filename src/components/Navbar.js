238c0ddb0e276b8877207bd94d15fcb1c2aab2ec
fede-blanco <blancofedem@gmail.com>
2022-10-24 12:55:31 -0300

    Navbar styles & change incons vectors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,170 +1,262 @@
-import React, {useEffect, useState} from "react"
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import theme, { breakpoints } from "../../utils/theme";
 import { StaticImage } from "gatsby-plugin-image";
-import { breakpoints } from "../../utils/theme";
-import Logo from '../images/logo-metaverse.png';
+import twitter from "../images/Vector-3.svg";
+import discord from "../images/Vector.svg";
+import openSea from "../images/Vector-openSea.svg";
 
 const Navbar = (props) => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-    const Links = [
-        {
-          label: 'ABOUT',
-          targetId: 'about'
-        },
-        {
-          label: 'LINEUP',
-          targetId: 'lineup'
-        },
-        {
-          label: 'MAP',
-          targetId: 'map'
-        },
-        // {
-        //   label: 'MERCH',
-        //   targetId: 'merch'
-        // },
-        // {
-        //   label: 'SPONSORS',
-        //   targetId: 'sponsors'
-        // },
-      ];
-    
-      const scrollTo = (targetId) => {
-        const target = document.getElementById(targetId);
-        if (target) {
-          target.scrollIntoView();
-        }
-      };
+  const Links = [
+    {
+      label: "ABOUT",
+      targetId: "about",
+    },
+    {
+      label: "LINEUP",
+      targetId: "lineup",
+    },
+    {
+      label: "MAP",
+      targetId: "map",
+    },
+    {
+      label: "FAQ",
+      targetId: "faq",
+    },
+    // {
+    //   label: 'SPONSORS',
+    //   targetId: 'sponsors'
+    // },
+  ];
+
+  const scrollTo = (targetId) => {
+    const target = document.getElementById(targetId);
+    if (target) {
+      target.scrollIntoView();
+    }
+  };
   return (
-    <StyledNavbar className="navbar">
-        <nav>
-            <StaticImage src={'./../images/logo-metaverse.svg'} height={140} />
-            {/* <ul>
-            {Links.map((link, i) => (
-              <li key={i}>
-                <a onClick={() => {scrollTo(link.targetId)}}>{link.label}</a>
-              </li>
-            ))}
-            </ul> */}
-        </nav>
-        <div className="mobile-toggle" onClick={() => {setIsMobileMenuOpen(!isMobileMenuOpen)}}>
+    <>
+      <StyledNavbar className="navbar">
+        <Nav>
+          <div>
+            <StaticImage src={"./../images/logo.svg"} height={140} />
+          </div>
+
+          <div>
+            <MenuList>
+              {Links.map((link, i) => (
+                <MenuItem key={i}>
+                  <a
+                    onClick={() => {
+                      scrollTo(link.targetId);
+                    }}
+                  >
+                    {link.label}
+                  </a>
+                </MenuItem>
+              ))}
+            </MenuList>
+          </div>
+          <div>
+            <a
+              href="https://twitter.com/decentraland"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SocialIcon src={twitter} />
+            </a>
+            <a
+              href="https://decentraland.org/discord/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SocialIcon src={discord} />
+            </a>
+            <a
+              href="https://www.instagram.com/decentraland_foundation/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <SocialIcon src={openSea} />
+            </a>
+          </div>
+        </Nav>
+        <div
+          className="mobile-toggle"
+          onClick={() => {
+            setIsMobileMenuOpen(!isMobileMenuOpen);
+          }}
+        >
           <div></div>
           <div></div>
           <div></div>
         </div>
-        <div className={`mobile-navbar ${isMobileMenuOpen ? 'open' : ''}`}>
-          <StaticImage onClick={() => {setIsMobileMenuOpen(!isMobileMenuOpen)}} className="close-menu-icon" src={'./../images/close_menu_icon.png'} alt="close" title="close" />
+        <div className={`mobile-navbar ${isMobileMenuOpen ? "open" : ""}`}>
+          <StaticImage
+            onClick={() => {
+              setIsMobileMenuOpen(!isMobileMenuOpen);
+            }}
+            className="close-menu-icon"
+            src={"./../images/close_menu_icon.png"}
+            alt="close"
+            title="close"
+          />
           <ul>
             {Links.map((link, i) => (
-                <li key={i}>
-                <a onClick={() => {
+              <li key={i}>
+                <a
+                  onClick={() => {
                     scrollTo(link.targetId);
                     setIsMobileMenuOpen(!isMobileMenuOpen);
-                  }}>{link.label}</a>
-                </li>
+                  }}
+                >
+                  {link.label}
+                </a>
+              </li>
             ))}
-            </ul>
+          </ul>
         </div>
-    </StyledNavbar>
-  )
-}
+      </StyledNavbar>
+    </>
+  );
+};
 
 const StyledNavbar = styled.section`
+  width: 100%;
+  z-index: 100;
+  position: fixed;
+
+  /* Agregado fede */
+  border-bottom: solid 3px ${theme.accent};
+  display: flex;
+  justify-content: space-between;
+  /* ---------------- */
+
+  @media screen and (max-width: ${breakpoints.md}) {
+    bottom: 0;
+    height: 52px;
+    background: #eefe56;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  &:before {
+    position: absolute;
+    bottom: 0;
+    height: 1px;
     width: 100%;
-    z-index: 100;
-    position: fixed;
+    content: "";
     @media screen and (max-width: ${breakpoints.md}) {
-      bottom: 0;
-      height: 52px;
-      background: #EEFE56;
-      display: flex;
-      justify-content: center;
-      align-items: center;
+      display: none;
     }
-    &:before {
-      position: absolute;
-      bottom: 0;
+  }
+  .mobile-toggle {
+    display: none;
+    width: 80px;
+    @media screen and (max-width: ${breakpoints.md}) {
+      display: unset;
+    }
+    div {
+      width: 80px;
       height: 1px;
-      width: 100%;
-      content: '';
+      background: black;
+      margin-bottom: 10px;
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+  }
+  .mobile-navbar {
+    width: 100vw;
+    height: 100vh;
+    bottom: -100vh;
+    position: absolute;
+    background: #eefe56;
+    opacity: 0;
+    pointer-events: none;
+    transition: 0.5s ease-in-out all;
+    padding: 24px;
+    .close-menu-icon {
+      width: 50px;
+      margin-bottom: 30px;
+    }
+    /* ul {
+      li {
+        font-size: 64px;
+        padding: 10px 0;
+        border-bottom: 1px solid black;
+        @media screen and (max-width: ${breakpoints.md}) {
+          font-size: 48px;
+        }
+        a {
+          -webkit-text-stroke: 1px black;
+          color: transparent;
+          &:hover {
+            color: black;
+          }
+        }
+      }
+    } */
+    &.open {
+      bottom: 0;
+      opacity: 1;
+      pointer-events: unset;
+    }
+  }
+  /* nav {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    ul {
+      display: flex;
+      justify-content: space-between;
+      padding: 12px 80px;
       @media screen and (max-width: ${breakpoints.md}) {
         display: none;
       }
-    }
-    .mobile-toggle {
-      display: none;
-      width: 80px;
-      @media screen and (max-width: ${breakpoints.md}) {
-        display: unset;
-      }
-      div {
-        width: 80px;
-        height: 1px;
-        background: black;
-        margin-bottom: 10px;
-        &:last-child {
-          margin-bottom: 0;
-        }
-      }
-    }
-    .mobile-navbar {
-      width: 100vw;
-      height: 100vh;
-      bottom: -100vh;
-      position: absolute;
-      background: #EEFE56;
-      opacity: 0;
-      pointer-events: none;
-      transition: 0.5s ease-in-out all;
-      padding: 24px;
-      .close-menu-icon {
-        width: 50px;
-        margin-bottom: 30px;
-      }
-      ul {
-        li {
-          font-size: 64px;
-          padding: 10px 0;
-          border-bottom: 1px solid black;
-          @media screen and (max-width: ${breakpoints.md}) {
-            font-size: 48px;
-          }
-          a {
-            -webkit-text-stroke: 1px black;
-            color: transparent;
-            &:hover {
-              color: black;
-            }
+      li {
+        font-size: 23px;
+        a {
+          cursor: pointer;
+          font-family: "SpaceMono";
+          opacity: 0.8;
+          &:hover {
+            opacity: 1;
           }
         }
       }
-      &.open {
-        bottom: 0;
-        opacity: 1;
-        pointer-events: unset;
-      }
     }
-    nav {
-      ul {
-        display: flex;
-        justify-content: space-between;
-        padding: 12px 80px;
-        @media screen and (max-width: ${breakpoints.md}) {
-          display: none;
-        }
-        li {
-          font-size: 23px;
-          a {
-            cursor: pointer;
-            font-family: 'SpaceMono';
-            opacity: 0.8;
-            &:hover {
-              opacity: 1;
-            }
-              }
-          }   
-      }
-  }
+  } */
+`;
+
+const SocialIcon = styled.img`
+  width: 24px;
+  height: 24px;
+  margin-left: 24px;
+`;
+
+const Nav = styled.nav`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 18px 62px;
+`;
+
+const MenuItem = styled.li`
+  font-family: "Yapari";
+  font-size: 16px;
+  font-weight: 700;
+  padding-left: 40px;
+`;
+
+const MenuList = styled.ul`
+  display: flex;
+  justify-content: space-between;
 `;
 
 export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,35 @@
-import React, { useState } from "react"
-import styled from 'styled-components'
-import { Helmet } from "react-helmet"
-import Layout from './../components/Layout'
-import Hero from './../components/Hero'
-import Banner from "../components/Banner"
-import StarsScene from "../components/Stars"
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Helmet } from "react-helmet";
+import Layout from "./../components/Layout";
+import Hero from "./../components/Hero";
+import Banner from "../components/Banner";
+// import StarsScene from "../components/Stars";
+import Navbar from "../components/Navbar";
 
 const seo = {
-  title: 'Metaverse Music Festival | Decentraland',
-  description: 'A four-day celebration of music, culture and creativity in the virtual social world of Decentraland, the Metaverse Festival is a grand collision of light, sound and portable toilets.',
-  image: 'https://themetaversefestival.io/logo-metaverse.png',
-}
+  title: "Metaverse Music Festival | Decentraland",
+  description:
+    "A four-day celebration of music, culture and creativity in the virtual social world of Decentraland, the Metaverse Festival is a grand collision of light, sound and portable toilets.",
+  image: "https://themetaversefestival.io/logo-metaverse.png",
+};
 
 const meta = {
   ...seo,
-  'og:title': seo.title || '',
-  'twitter:title': seo.title || '',
-  'og:description': seo.description || '',
-  'twitter:description': seo.description || '',
-  'og:image': seo.image || '',
-  'twitter:image': seo.image || '',
-  'twitter:card': 'summary_large_image',
-  'twitter:creator': '@decentraland',
-  'og:type': 'website',
-}
+  "og:title": seo.title || "",
+  "twitter:title": seo.title || "",
+  "og:description": seo.description || "",
+  "twitter:description": seo.description || "",
+  "og:image": seo.image || "",
+  "twitter:image": seo.image || "",
+  "twitter:card": "summary_large_image",
+  "twitter:creator": "@decentraland",
+  "og:type": "website",
+};
 
 // markup
 const IndexPage = () => {
-  const [showVideo, setShowVideo] = useState(true)
+  const [showVideo, setShowVideo] = useState(false);
 
   return (
     <StyledIndexPage>
@@ -35,41 +37,51 @@ const IndexPage = () => {
         {Object.keys(meta)
           .filter((name) => Boolean(meta[name]))
           .map((name) => {
-            if (name === 'title') {
-              return <title key={name}>{meta[name]}</title>
+            if (name === "title") {
+              return <title key={name}>{meta[name]}</title>;
             }
 
-            if (name.startsWith('og:')) {
-              return <meta key={name} property={name} content={meta[name]} />
+            if (name.startsWith("og:")) {
+              return <meta key={name} property={name} content={meta[name]} />;
             }
 
-            return <meta key={name} name={name} content={meta[name]} />
+            return <meta key={name} name={name} content={meta[name]} />;
           })}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff"></meta>
       </Helmet>
       <Layout>
         <header>
+          <Navbar />
           <Hero />
         </header>
-        <main>
-          {
-            showVideo &&
-            <Banner setShowVideo={setShowVideo} />
-          }
-        </main>
+        <main>{showVideo && <Banner setShowVideo={setShowVideo} />}</main>
       </Layout>
-      <StarsScene />
+      {/* <StarsScene /> */}
     </StyledIndexPage>
-  )
-}
+  );
+};
 
 const StyledIndexPage = styled.div`
   background: transparent;
-`
+`;
 
-export default IndexPage
+export default IndexPage;
