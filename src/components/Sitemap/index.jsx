6c30be50c526de3e7286d5da5d28391420b4b4c6
franzo <franciscoaboy@gmail.com>
2022-11-08 13:46:14 -0300

    interactive sitemap svg

--- src/components/Sitemap/index.jsx
+++ src/components/Sitemap/index.jsx
@@ -1,18 +1,15 @@
 import React from "react";
-import { Section, Title, Map } from "./styles";
+import { Section, Title } from "./styles";
 import title from '../../images/sitemap-title.png';
 import { Container } from "../Container";
 import { StaticImage } from "gatsby-plugin-image";
+import { Map } from "./sitemap";
 
 const Sitemap = () => (
     <Section id="sitemap">
         <Container>
             <Title src={title} />
-            <Map>
-                <StaticImage
-                    src={'./../../images/sitemap.svg'}
-                />
-            </Map>
+            <Map />
         </Container>
     </Section>
 )
--- src/components/Sitemap/styles.js
+++ src/components/Sitemap/styles.js
@@ -35,3 +35,33 @@ export const Map = styled.div`
         padding: 0 120px;
     }
 `
+
+export const Stage = styled.g`
+    polygon, path {
+        transition: all 0.4s ease;
+    }
+
+    &:hover {
+        path.st0 {
+            fill: #000000;
+        }
+        polygon {
+            fill: url(#metallic-pattern);
+            filter: drop-shadow( 0px 0 10px hsla(0, 100%, 100%, 1));
+        }
+    }
+`
+
+export const SVG = styled.svg`
+    .st0{fill:#FFFFFF;}
+	.st1{fill:none;stroke:#FFFFFF;stroke-width:6;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
+	.st2{fill:none;stroke:#FFFFFF;stroke-width:5.5092;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
+    .st3{fill:none;stroke:#FFFFFF;stroke-width:5.6189;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
+    .st4{fill:none;stroke:#FFFFFF;stroke-width:4;stroke-miterlimit:10;}
+`
+
+export const Grid = styled.g`
+    rect:hover {
+        fill: #fff;
+    }
+`
