ab103227aac42364a9438a23243051af4f7bfbe0
fede-blanco <blancofedem@gmail.com>
2022-11-14 15:48:05 -0300

    Add MerchBanner folder & styles

--- src/components/MerchBanner/index.jsx
+++ src/components/MerchBanner/index.jsx
@@ -1,22 +1,10 @@
-import React from "react"
-import styled from "styled-components"
+import React from "react";
+import { Link, Banner } from "./styles";
 
-export const MerchBanner = ({ src, href }) => (
-    <Link href={href} target="_blank">
-        <Banner src={src} />
-    </Link>
-)
+const MerchBanner = ({ src, href }) => (
+  <Link href={href} target="_blank">
+    <Banner src={src} />
+  </Link>
+);
 
-const Link = styled.a`
- cursor: crosshair;
-`
-
-const Banner = styled.img`
-    width: 100%;
-    height: auto;
-    object-fit: cover;
-    font-family: "Yapari";
-`
-
-
-export default MerchBanner
+export default MerchBanner;
---
+++ src/components/MerchBanner/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Link = styled.a`
+  cursor: crosshair;
+`;
+
+export const Banner = styled.img`
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+  font-family: "Yapari";
+`;
